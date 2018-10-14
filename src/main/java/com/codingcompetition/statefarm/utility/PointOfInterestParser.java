e271ecde28701128b868aa06d334b83b04fd0fc2
Ananth Dandibhotla <anisaidan@gmail.com>
2018-10-13 23:00:44 -0400

    Make pointofinterestparser set values to the lat,long, and other tag values.

--- src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
+++ src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestParser.java
@@ -11,9 +11,13 @@ import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class PointOfInterestParser {
 
@@ -28,7 +32,7 @@ public class PointOfInterestParser {
         try {
             SAXParser saxParser = saxParserFactory.newSAXParser();
             PointHandler handler = new PointHandler();
-            saxParser.parse(new File("/Users/pankaj/employees.xml"), handler);
+            saxParser.parse(new File("src/main/resources" + fileName), handler);
             return handler.getPoints();
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
@@ -37,34 +41,44 @@ public class PointOfInterestParser {
     }
 
     //Handler to parse data with SAX
-    private class PointHandler extends DefaultHandler{
+    private class PointHandler extends DefaultHandler {
         private List<PointOfInterest> pointsOfInterest;
         private PointOfInterest currentPoint;
 
         @Override
         public void startDocument() throws SAXException {
             super.startDocument();
-            pointsOfInterest =  new ArrayList<>();
+            pointsOfInterest = new ArrayList<>();
         }
 
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-            currentPoint = new PointOfInterest();
-            if(qName.toLowerCase().equals("node")){
+            //System.out.println(qName);
+            if (qName.toLowerCase().equals("node")) {
+                currentPoint = new PointOfInterest();
                 for (int i = 0; i < attributes.getLength(); i++) {
-                    currentPoint.addDescriptor(attributes.getQName(i),attributes.getValue(i));
+                    if (attributes.getQName(i).equals("lat")) {
+                        currentPoint.setLatitude(attributes.getValue(i));
+                    } else if (attributes.getQName(i).equals("lon")) {
+                        currentPoint.setLongitude(attributes.getValue(i));
+                    } else {
+                        currentPoint.addDescriptor(attributes.getQName(i), attributes.getValue(i));
+                    }
                 }
+            }
+            if (qName.toLowerCase().equals("tag")) {
+                currentPoint.addDescriptor(attributes.getValue(0),attributes.getValue(1));
             }
         }
 
         @Override
         public void endElement(String uri, String localName, String qName) throws SAXException {
-            if(qName.toLowerCase().equals("node")){
+            if (qName.toLowerCase().equals("node")) {
                 pointsOfInterest.add(currentPoint);
             }
         }
 
-        public List<PointOfInterest> getPoints(){
+        public List<PointOfInterest> getPoints() {
             return pointsOfInterest;
         }
     }
