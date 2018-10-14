7d6aa430f049b1bd37d8bd4496daf42f5a192a7a
Aubhro Sengupta <aubhros@gmail.com>
2018-10-13 21:40:46 -0400

    Add getters and setters to PointOfInterest

--- src/main/java/com/codingcompetition/statefarm/model/PointOfInterest.java
+++ src/main/java/com/codingcompetition/statefarm/model/PointOfInterest.java
@@ -5,15 +5,35 @@ import java.util.Map;
 
 public class PointOfInterest {
 
+    private Map<Object,String> descriptors;
+    private String latitude;
+    private String longitude;
+
+    public PointOfInterest() {
+        descriptors = new HashMap<>();
+    }
+
     public Map<Object,String> getDescriptors() {
-    return new HashMap<>();
+        return this.descriptors;
     }
 
     public String getLatitude() {
-        return "";
+        return this.latitude;
     }
 
     public String getLongitude() {
-        return "";
+        return this.longitude;
+    }
+
+    public void addDescriptor(Object key, String value) {
+        descriptors.put(key, value);
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
     }
 }
