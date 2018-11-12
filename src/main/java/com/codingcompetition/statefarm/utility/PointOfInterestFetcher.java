e40c42f4e10a38454331c0211af665f93261fd69
Aubhro Sengupta <aubhros@gmail.com>
2018-11-12 01:46:47 -0600

    Cache points of interest for latitude and longitude

--- src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestFetcher.java
+++ src/main/java/com/codingcompetition/statefarm/utility/PointOfInterestFetcher.java
@@ -8,24 +8,32 @@ public class PointOfInterestFetcher {
     private String pointOfInterestQuery =  "https://api.openstreetmap.org/api/0.6/map?bbox=%f,%f,%f,%f";
 
     public void fetchXML(double latitude, double longitude, double boxSize) throws Exception {
-        double edgeDistance = boxSize / 2;
-        URL url  = new URL(String.format(pointOfInterestQuery,
-                longitude - edgeDistance,
-                latitude - edgeDistance,
-                longitude + edgeDistance,
-                latitude + edgeDistance));
 
-        URLConnection urlConnection = url.openConnection();
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                        urlConnection.getInputStream()));
-        String inputLine;
-        FileWriter writer = new FileWriter(new File("src/main/resources/poi.xml"), false);
-        while ((inputLine = in.readLine()) != null) {
-            writer.append(inputLine);
+        String outFilePathName = String.format("cache/poi/%f-%f-%f.xml", latitude, longitude, boxSize);
+        File file = new File(outFilePathName);
+
+        if (!file.exists() || file.isDirectory()) {
+            double edgeDistance = boxSize / 2;
+            URL url = new URL(String.format(pointOfInterestQuery,
+                    longitude - edgeDistance,
+                    latitude - edgeDistance,
+                    longitude + edgeDistance,
+                    latitude + edgeDistance));
+
+            URLConnection urlConnection = url.openConnection();
+
+            FileWriter writer = new FileWriter(file, false);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            urlConnection.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                writer.append(inputLine);
+            }
+
+            in.close();
+            writer.close();
         }
-        in.close();
-        writer.close();
     }
 
     public static void main(String[] args) {
